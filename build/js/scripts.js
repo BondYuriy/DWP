223c01dab1df86569aad8a66d007bf524f43f122
BondYuriy <bondar.y@i.ua>
2018-12-13 22:05:02 +0200

    adds main home page

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,18 +1,18 @@
-$(function() {
-  $(window).scroll(function() {
-    var winTop = $(window).scrollTop();
-    if (winTop >= 30) {
-      $("body").addClass("sticky-header");
-    } else {
-      $("body").removeClass("sticky-header");
-    } //if-else
-  }); //win func.
-}); //ready func.
-
-//============ menu btn ==============
-
-$('.navbar-toggler').click(function(){
-  $(this).toggleClass('open');
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1920: {
+        items: 1
+      }
+    }
+  });
 });
-
-//====================================
